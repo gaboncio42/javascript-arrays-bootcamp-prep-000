50aec081b2d7c4977b54ef79e96a283af9bd5836
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-14 11:55:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,1 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-function addElementToBeginningOfArray (array, element) {
-  var beginning = [1];
-  return beginning.unshift['foo'];
-}
-
-function destructivelyAddElementToBeginningOfArray (array, elemet) {
-  var beginning = [1];
-  return ('foo',...beginning);
-}
