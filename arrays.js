2fa8734f814a75c21fc3cf6c52193f56efa74910
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-14 12:56:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (array, element) {
   var beginning = "foo";
-  return ("foo",...beginning);
+  return ("foo",...chocolateBars);
 }
