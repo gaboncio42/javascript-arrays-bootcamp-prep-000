6f1c8c8907b2880798d25d0fd41d8fe15473ae28
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-14 23:33:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
-var chocolateBars = ["snickers", "hundred "]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var newCandy = "foo";
+
+function addElementToBeginningOfArray (chocolateBars, newCandy) {
+  return [newCandy, ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray (chocolateBars, newCandy) {
+  chocolateBars.unshift(newBar);
+  return chocolateBars;
+}
