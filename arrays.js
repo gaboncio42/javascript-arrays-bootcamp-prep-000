eb8e7dd144f60624fea1bf3fe4d61a717b3ae617
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-14 12:14:24 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var newBar = ["foo"];
+
+function addElementToBeginningOfArray(chocolateBars, newBar) {
+  return ("newBar",..."chocolateBars");
+}
